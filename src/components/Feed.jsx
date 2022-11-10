00be5a8554a418ff4ab5fc45a2f3729d9d1d3383
sapey <alexandre.froment92@gmail.com>
2022-11-10 11:14:12 +0100

    prepare the call from api suggested video and change name of category

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,26 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { SideBar, Videos } from '.';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+  }, [selectedCategory])
+
   return (
     <Stack sx={{flexDirection: { sx: 'column', md: 'row' }}}>
       <Box sx={{ height:{ sx: 'auto', md:'92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
-        <SideBar />
+
+        <SideBar 
+          selectedCategory = {selectedCategory}
+          setSelectedCategory = {setSelectedCategory}
+        />
+
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright de ta grosse mère
         </Typography>
@@ -15,7 +28,7 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white'}}>
-          New <span style={{ color: '#F31503'}}>videos</span>
+          {selectedCategory} <span style={{ color: '#F31503'}}>videos</span>
         </Typography>
 
         <Videos videos={[]}/>
