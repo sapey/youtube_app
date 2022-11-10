18443a14a274179a96cedb24266569bbd232609e
sapey <alexandre.froment92@gmail.com>
2022-11-10 23:37:26 +0100

    start to organize video feed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
 
-const VideoCard = () => {
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
+import { height } from '@mui/system';
+
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
+    console.log(videoId, snippet);
   return (
-    <div>VideoCard</div>
+    <Card>
+        <Link to ={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <CardMedia 
+                image={snippet?.thumbnails?.high?.url}
+                alt={snippet?.title}
+                sx={{ width: 358, height: 180 }}
+            />
+        </Link>
+        <CardContent />
+    </Card>
   )
 }
 
