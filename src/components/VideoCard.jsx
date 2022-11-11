29acff75a79546c1c499678a278b138b0b9c5cc8
sapey <alexandre.froment92@gmail.com>
2022-11-11 16:26:30 +0100

    create link of video and channel on card on the feed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,7 +8,7 @@ import { height } from '@mui/system';
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
     console.log(videoId, snippet);
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 'none' }}>
         <Link to ={videoId ? `/video/${videoId}` : demoVideoUrl}>
             <CardMedia 
                 image={snippet?.thumbnails?.high?.url}
@@ -16,7 +16,19 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
                 sx={{ width: 358, height: 180 }}
             />
         </Link>
-        <CardContent />
+        <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px'}}>
+            <Link to ={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+                    {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+                </Typography>
+            </Link>
+            <Link to ={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+                <Typography variant="subtitle2" fontWeight="bold" color="gray">
+                    {snippet?.channelTitle || demoChannelTitle }
+                    <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px'}}/>
+                </Typography>
+            </Link>
+        </CardContent>
     </Card>
   )
 }
