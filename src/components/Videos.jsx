5c36f1cfe93f89a0b233886b122474b80fe2d2d3
sapey <alexandre.froment92@gmail.com>
2022-11-16 23:11:01 +0100

    finish project youtube clone

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,6 +3,9 @@ import { Stack, Box } from '@mui/material';
 import { ChannelCard, VideoCard } from '.';
 
 const Videos = ({ videos, direction }) => {
+
+  if (!videos?.length) return 'Loading...';
+
   return (
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
         {videos.map((item, idx) => (
